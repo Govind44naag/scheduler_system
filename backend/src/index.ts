5b409c21d23611cc31970f3fc0a5d46a0d891f37
Govind Naag <govindnaag44@gmail.com>
2025-09-05 08:33:45 +0530

    add url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin:'http://localhost:3000',
+  origin:'https://scheduler-system-alpha.vercel.app',
   credentials:true,
 }));
 app.use(morgan('combined'));
