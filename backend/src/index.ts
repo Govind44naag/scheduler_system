f448e6118fbb2e10553136940ac5951a1ef4712d
Govind Naag <govindnaag44@gmail.com>
2025-09-05 21:31:46 +0530

    add cors opt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,10 +13,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({
-  origin:'https://scheduler-system-alpha.vercel.app',
-  credentials:true,
-}));
+app.use(
+  cors({
+    origin: ["https://scheduler-system-alpha.vercel.app"], // your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(morgan('combined'));
 app.use(express.json());
 
